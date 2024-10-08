b56e97653e6b11be21b5b960b5ce1506fb544ffb
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-08 23:53:25 +0300

    pointer to pointer lab ex3 was done

--- pointer_to_pointer/main.cpp
+++ pointer_to_pointer/main.cpp
@@ -1,9 +1,79 @@
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
-int main()
-{
-	
+template<typename T>
+T** create2DArray(int row, int col) {
+    T** matrix = new T * [row];
+    for (int i = 0; i < row; i++) {
+        matrix[i] = new T[col];
+        for (int j = 0; j < col; j++) {
+            matrix[i][j] = rand() % 20;
+        }
+    }
+    return matrix;
+}
 
-	return 0;
+template<typename T>
+void print2DArray(T** matrix, int row, int col) {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+template<typename T>
+T** addRowToArray(T** matrix, int& rows, int cols, int pos) {
+    if (pos < 0 || pos > rows) {
+        cout << "Invalid position!" << endl;
+        return matrix; 
+    }
+
+    T** newMatrix = new T * [rows + 1];
+    for (int i = 0; i < pos; i++) {
+        newMatrix[i] = matrix[i]; 
+    }
+    newMatrix[pos] = new T[cols];
+    for (int j = 0; j < cols; j++) {
+        newMatrix[pos][j] = rand() % 20;
+    }
+    for (int i = pos; i < rows; i++) {
+        newMatrix[i + 1] = matrix[i]; 
+    }
+
+    rows++; 
+    delete[] matrix; 
+    return newMatrix; 
+}
+
+int main() {
+    srand(time(0));
+    int rows, cols;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    int** matrix = create2DArray<int>(rows, cols);
+    cout << "Original array:" << endl;
+    print2DArray(matrix, rows, cols);
+
+    int pos;
+    cout << "Enter the position where you want to add a new row: ";
+    cin >> pos;
+
+    matrix = addRowToArray(matrix, rows, cols, pos);
+    cout << "Array after adding a new row:" << endl;
+    print2DArray(matrix, rows, cols);
+
+    for (int i = 0; i < rows; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
 }
